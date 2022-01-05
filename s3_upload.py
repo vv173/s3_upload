12c8ba0e9b741761efc28aae8072fafa399340c3
Viktor Vodnev <v.vodnev73@gmail.com>
2022-01-05 16:05:47 +0100

    Argument parsing added

--- s3_upload.py
+++ s3_upload.py
@@ -7,6 +7,18 @@
 
 import boto3
 import pathlib
+import hashlib
+import argparse
+
+def arguments():
+    parser = argparse.ArgumentParser(description='Start parsing arguments')
+    parser.add_argument('--s3url', type=str, help='Provide a link to the s3 storage', required=True)
+    parser.add_argument('--s3key', type=str, help='KEY for s3 Storage', required=True)
+    parser.add_argument('--s3secret', type=str, help='SECRET for s3 Storage', required=True)
+    parser.add_argument('-f', type=str, help='Path to file to upload', required=True)
+    parser.add_argument('--s3path', type=str, help='Path to s3 object', required=True)
+    args = parser.parse_args()
+    return args
 
 def upload(file_path, s3url, s3_path, S3KEY, S3SECRET):
     
@@ -15,15 +27,24 @@ def upload(file_path, s3url, s3_path, S3KEY, S3SECRET):
         'aws_secret_access_key':  S3SECRET,
         'endpoint_url': s3url,
         'verify': False
+        #'use_ssl': True
     }
     path_split = pathlib.Path(s3_path)
     
-    s3 = boto3.client('s3', **client_kwargs)
+    s3_client = boto3.client('s3', **client_kwargs)
     remote_path = str(pathlib.Path(*path_split.parts[1:]))
-    s3.upload_file(file_path, path_split.parts[0], remote_path)
+    s3_client.upload_file(file_path, path_split.parts[0], remote_path)
+
+def md5_verification(file_path):
+    with open(file_path, 'rb') as file_to_verif:
+        bytes = file_to_verif.read()
+        md5_local = hashlib.md5(bytes).hexdigest()
+    print(md5_local)
 
 def main():
-    upload("file.txt", "http://192.168.169.12:9000", "test/test2/file.txt", "miniokey", "miniosecret")
+    args = arguments();
+    upload(args.f, args.s3url, args.s3path, args.s3key, args.s3secret)
+    md5_verification(args.f)
 
 if __name__ == '__main__':
     main()
